3695553db8ee44439d49e74de9b41a5a098a42f8
December G <dgarnetsmith@gmail.com>
2023-06-24 10:29:06 -0500

    Update favicon and hide twitter login bar

--- scripts/background.js
+++ scripts/background.js
@@ -1,15 +1,13 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   let tabUrl = tab.url ?? tab.pendingUrl;
-  let fullUrl = tabUrl.split('?')
-  const queryParams = fullUrl[1]
-  let domainOnlyUrl = fullUrl[0];
   if (
     changeInfo.status === 'complete' &&
     tabUrl &&
-    tabUrl.includes('www.reddit.com')
+    tabUrl.includes('twitter.com')
   ) {
-    const tedditUrl = domainOnlyUrl.replace('reddit.com', 'teddit.net')
-    const newUrl = queryParams ? `${tedditUrl}?${queryParams}` : tedditUrl;
-    chrome.tabs.update(tabId, { url: newUrl });
+     chrome.scripting.insertCSS({
+      target: { tabId: tabId },
+      files: ['css/global.css'],
+    });
   }
 });
